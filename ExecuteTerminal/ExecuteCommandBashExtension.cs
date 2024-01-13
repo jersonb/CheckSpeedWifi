2c48b8ff632c026df129cce7fd70a22eca6b4170
jersonb <jersonlbrito@hotmail.com>
2024-01-13 19:43:31 -0300

    feat: execute command bash extensionn

---
+++ ExecuteTerminal/ExecuteCommandBashExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace ExecuteTerminal
+{
+    public static class ExecuteCommandBashExtension
+    {
+        public static string ExecuteCommandBash(this string command)
+        {
+            command = command.Replace("\"", "\"\"");
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = @$"-c {command}",
+                    UseShellExecute = false,
+                    RedirectStandardInput = true,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            return process.StandardOutput.ReadToEnd();
+        }
+    }
+}
